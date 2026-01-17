c1776a99ac11bffaa825bc7a6425529de35dab36
maryhelengonzalez <maryhelengonzalez51@gmail.com>
2026-01-17 22:42:52 +0000

    Add CMake build system and update CI to use CMake

---
+++ build/CMakeFiles/test_threadmanager.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_threadmanager.dir/src/ThreadManager.cpp.o"
+  "CMakeFiles/test_threadmanager.dir/src/ThreadManager.cpp.o.d"
+  "CMakeFiles/test_threadmanager.dir/tests/test_threadmanager.cpp.o"
+  "CMakeFiles/test_threadmanager.dir/tests/test_threadmanager.cpp.o.d"
+  "test_threadmanager"
+  "test_threadmanager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_threadmanager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
